368a0ec9274da4ab6be0acb6020b6ee47c0a4eea
Martin Filliau <martin@filliau.com>
2014-05-19 17:37:20 +0100

    Basic admin interface

--- talks/events/admin.py
+++ talks/events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Series, Talk, Speaker
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start', 'end')
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Talk, EventAdmin)
+admin.site.register(Speaker, SpeakerAdmin)
