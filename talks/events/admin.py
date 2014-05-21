e66df929b641dfc550d982db18b74229ef48a946
Martin Filliau <martin@filliau.com>
2014-05-21 18:02:58 +0100

    Admin location

--- talks/events/admin.py
+++ talks/events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, Series, Talk, Speaker
+from .models import Event, Series, Talk, Speaker, Location
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -15,7 +15,12 @@ class SeriesAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
 
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('oxpoints_id', 'name',)
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Series, SeriesAdmin)
 admin.site.register(Talk, EventAdmin)
 admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Location, LocationAdmin)
