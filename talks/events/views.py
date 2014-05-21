12c0ee68df29999c88cde9ea1f11cefd1403365a
Martin Filliau <martin@filliau.com>
2014-05-21 14:06:34 +0100

    draft initial views

--- talks/events/views.py
+++ talks/events/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, 'events/events.html', {})
+
+
+def upcoming_events(request):
+    pass
+
+
+def events_for_year(request, year):
+    pass
+
+
+def events_for_month(request, year, month):
+    pass
+
+
+def events_for_day(request, year, month, day):
+    pass
+
+
+def event(request, event_id):
+    pass
--- talks/urls.py
+++ talks/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'talks.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from events.views import (homepage, upcoming_events, event, events_for_day,
+                          events_for_month, events_for_year)
 
+
+urlpatterns = patterns('',
+    url(r'^$', homepage, name='homepage'),
+    url(r'^events$', upcoming_events, name='upcoming_events'),
+    url(r'^events/id/(?P<event_id>\d+)$', event, name='event'),
+    url(r'^events/(?P<year>\d{4})/$', events_for_year, name='events_year'),
+    url(r'^events/(?P<year>\d{4})/(?P<month>\d{2})/$', events_for_month, name='events_month'),
+    url(r'^events/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', events_for_day, name='events_day'),
     url(r'^admin/', include(admin.site.urls)),
 )
