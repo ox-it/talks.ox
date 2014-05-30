9dd819246e325e333cb8425857d9197ae28e9a10
David King <dave@davbo.org>
2014-05-30 17:41:38 +0100

    Updated model / migration for Event/EventGroup
    Essentially removes the "Series" and replaces with a more generic
    "EventGroup".

--- talks/events/models.py
+++ talks/events/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 from talks.api_ox.models import Location, Organisation
 
 
-class Series(models.Model):
+class EventGroup(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField()
     description = models.TextField()
@@ -29,36 +29,17 @@ class Event(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField()
     description = models.TextField()
+    speakers = models.ManyToManyField(Speaker, null=True, blank=True)
     # TODO audience should it be free text
     # TODO booking information; structure?
 
-    series = models.ForeignKey(Series, null=True, blank=True)
+    group = models.ForeignKey(EventGroup, null=True, blank=True)
     location = models.ForeignKey(Location, null=True, blank=True)
     # TODO I'm guessing an event can be organised by multiple departments?
     department_organiser = models.ForeignKey(Organisation, null=True, blank=True)
 
     def __unicode__(self):
         return "Event: {title} ({start})".format(title=self.title,
-                                          start=self.start.strftime("%Y-%m-%d %H:%M"))
-
-
-class Talk(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField(null=True, blank=True)
-    title = models.CharField(max_length=250)
-    slug = models.SlugField()
-    description = models.TextField()
-    # TODO audience should it be free text
-    # TODO booking information; structure?
-
-    event = models.ForeignKey(Event)
-    speaker = models.ManyToManyField(Speaker, null=True, blank=True)
-    location = models.ForeignKey(Location, null=True, blank=True)
-    department_organiser = models.ForeignKey(Organisation, null=True, blank=True)
-
-
-    def __unicode__(self):
-        return "Talk: {title} ({start})".format(title=self.title,
                                           start=self.start.strftime("%Y-%m-%d %H:%M"))
 
 
@@ -79,4 +60,4 @@ class TagItem(models.Model):
     tag = models.ForeignKey(Tag)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    item = GenericForeignKey('content_type', 'object_id')   # atm: Talk, Event, Series
+    item = GenericForeignKey('content_type', 'object_id')   # atm: Event, EventGroup
