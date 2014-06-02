b3768051c8a9e8aedbf657fec7d7e7b4a64a23fa
Martin Filliau <martin@filliau.com>
2014-06-02 12:07:30 +0100

    Fixed recursion bug introduced in 910eaefa61808ca65237f088846bc59cf9f0c6c8

--- talks/api_ox/api.py
+++ talks/api_ox/api.py
@@ -14,7 +14,7 @@ class ApiOxResource(object):
 
     def _get_request(self, path, params=None):
         try:
-            r = self._get_request(path)
+            r = requests.get('{base_url}{path}'.format(base_url=self.base_url, path=path), timeout=self.timeout)
             if r.status_code == requests.codes.ok:
                 return r.json()
             elif r.status_code == requests.codes.not_found:

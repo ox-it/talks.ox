ox-it/talks.ox

commit 910eaefa61808ca65237f088846bc59cf9f0c6c8

Added call to fetch organisational descendants

talks/api_ox/api.py
@@ -13,16 +13,8 @@ class ApiOxResource(object):
         self.timeout = timeout
 
     def _get_request(self, path, params=None):
-        return requests.get('{base_url}{path}'.format(base_url=self.base_url, path=path), timeout=self.timeout)
-
-
-class PlacesResource(ApiOxResource):
-
-    PATH = '/places/'
-
-    def get_by_id(self, ident):
         try:
-            r = self._get_request('{path}{ident}'.format(path=self.PATH, ident=ident))
+            r = self._get_request(path)
             if r.status_code == requests.codes.ok:
                 return r.json()
             elif r.status_code == requests.codes.not_found:
@@ -33,6 +25,17 @@ class PlacesResource(ApiOxResource):
         except RequestException as re:
             logger.error("Unable to reach the API", exc_info=True)
             raise ApiException()
+
+
+class PlacesResource(ApiOxResource):
+
+    PATH = '/places/'
+
+    def get_by_id(self, ident):
+        return self._get_request('{path}{ident}'.format(path=self.PATH, ident=ident))
+
+    def get_organisation_descendants(self, ident):
+        return self._get_request('{path}{ident}/organisation-descendants'.format(path=self.PATH, ident=ident))
 
 
 class ApiException(Exception):

talks/api_ox/query.py
@@ -4,7 +4,8 @@ from .api import PlacesResource
 
 
 def find_org_descendants(oxpoints_id):
-    pass
+    api = PlacesResource(settings.API_OX_URL)
+    return api.get_organisation_descendants('oxpoints:{ident}'.format(ident=oxpoints_id))
 
 
 def find_spatial_descendants(oxpoints_id):

commit b3768051c8a9e8aedbf657fec7d7e7b4a64a23fa

Fixed recursion bug introduced in 910eaefa61808ca65237f088846bc59cf9f0c6c8

talks/api_ox/api.py
@@ -14,7 +14,7 @@ class ApiOxResource(object):
 
     def _get_request(self, path, params=None):
         try:
-            r = self._get_request(path)
+            r = requests.get('{base_url}{path}'.format(base_url=self.base_url, path=path), timeout=self.timeout)
             if r.status_code == requests.codes.ok:
                 return r.json()
             elif r.status_code == requests.codes.not_found: