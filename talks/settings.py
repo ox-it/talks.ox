7173ca366855ec89b8786bb9c0cfa2241390eb91
David King <dave@davbo.org>
2014-06-02 12:08:49 +0100

    Integrate bootstrap with existing templates

--- talks/settings.py
+++ talks/settings.py
@@ -84,6 +84,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_URL = '/static/'
 
 
