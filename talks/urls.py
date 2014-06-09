4103145e7e01bb8164ce60d1d928c0520f59a4ae
Martin Filliau <martin@filliau.com>
2014-06-09 11:51:09 +0100

    Removed date facetting for now -- we need date range to be implemented

--- talks/urls.py
+++ talks/urls.py
@@ -14,7 +14,8 @@ from api.views import EventViewSet
 router = routers.DefaultRouter()
 router.register(r'events', EventViewSet)
 
-sqs = SearchQuerySet().facet('speakers', mincount=1).facet('locations', mincount=1).date_facet('start', date(2014,1,1), date(2015,1,1), 'month')
+sqs = SearchQuerySet().facet('speakers', mincount=1).facet('locations', mincount=1)
+# .date_facet('start', date(2014,1,1), date(2015,1,1), 'month')   removed for now as we want dynamic dates and range
 
 
 urlpatterns = patterns('',
