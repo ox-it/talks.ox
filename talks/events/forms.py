347faf7f550253bb076accbb1c4ecaba9d906324
Dave King <dave@davbo.org>
2014-06-10 11:44:20 +0100

    Validate between the Select and create new EventGroup
    Added simple booleans to manage this selection for now

--- talks/events/forms.py
+++ talks/events/forms.py
@@ -20,8 +20,8 @@ class EventForm(forms.ModelForm):
 
 class EventGroupForm(forms.ModelForm):
     enabled = forms.BooleanField(label='Add to a group?')
-    form_enabled = forms.CharField(widget=forms.HiddenInput)
-    select_enabled = forms.CharField(widget=forms.HiddenInput)
+    form_enabled = forms.BooleanField(required=False)
+    select_enabled = forms.BooleanField(required=False)
     event_group_select = forms.ModelChoiceField(
             queryset=EventGroup.objects.all(),
             required=False,
@@ -30,3 +30,33 @@ class EventGroupForm(forms.ModelForm):
     class Meta:
         fields = ('form_enabled', 'select_enabled', 'event_group_select', 'title', 'description')
         model = EventGroup
+
+    def clean(self):
+        cleaned_data = super(EventGroupForm, self).clean()
+        if 'enabled' in cleaned_data:
+            if 'form_enabled' in cleaned_data:
+                return cleaned_data
+            elif 'select_enabled' in cleaned_data:
+                self.errors['title'] = None
+                self.errors['description'] = None
+                if not cleaned_data.get('event_group_select', None):
+                    self.add_error('event_group_select', "Select an Event Group")
+                return cleaned_data
+        else:
+            return {}
+
+
+    def get_event_group(self):
+        # Form has been completed and user has selected an event group
+        valid = self.is_valid()
+        if 'enabled' in self.cleaned_data:
+            # Creating a new EventGroup
+            if valid and 'form_enabled' in self.cleaned_data:
+                return self.save()
+            elif 'select_enabled' in self.cleaned_data and 'event_group_select' in self.cleaned_data:
+                return self.cleaned_data['event_group_select']
+        return None
+
+    def is_enabled(self):
+        self.is_valid()
+        return 'enabled' in self.cleaned_data
