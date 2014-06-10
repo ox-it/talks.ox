7e1d7b3dc0c320ce968181750480508b5f26e558
Martin Filliau <martin@filliau.com>
2014-06-10 11:41:57 +0100

    Added suggestions field (copy some fields to be available for spellchecking)

--- talks/settings.py
+++ talks/settings.py
@@ -124,6 +124,7 @@ REST_FRAMEWORK = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr/collection1'
+        'URL': 'http://127.0.0.1:8983/solr/collection1',
+        'INCLUDE_SPELLING': True
     },
 }
