b48bd042474f6451df59e47040f3b373d9466a95
David King <dave@davbo.org>
2014-06-27 17:22:08 +0100

    Remove the typeahead and only use bloodhound

--- static/js/speaker-typeahead.js
+++ static/js/speaker-typeahead.js
@@ -9,12 +9,30 @@ $(function() {
             },
         }
     });
-
     speakersBH.initialize();
 
-    $('.js-speakers-typeahead').typeahead(null, {
-        name: 'speakers',
-        displayKey: 'name',
-        source: speakersBH.ttAdapter()
+    var speakerTemplate = _.template('<a href="#" data-id="<%= id %>" class="js-speaker-suggestion list-group-item"><span class="badge">+</span><%= name %></a>');
+    var $suggestedSpeakers = $('.js-suggested-speakers-list');
+    var cachedSuggestions = {};
+    $('.js-speakers-typeahead').on('keyup', function(ev) {
+        var query = ev.target.value;
+        speakersBH.get(query, function(suggestions) {
+            $suggestedSpeakers.html('');
+            _.each(suggestions, function(suggestion) {
+                cachedSuggestions[suggestion.id] = suggestion;
+                $suggestedSpeakers.append(speakerTemplate(suggestion));
+            });
+        });
+    });
+    $suggestedSpeakers.on('click', '.js-speaker-suggestion', function(ev) {
+        ev.preventDefault();
+        var $suggestion = $(ev.target);
+        $suggestion.addClass('disabled');
+        var speakerID = $suggestion.data('id');
+        var speaker = cachedSuggestions[speakerID];
+        document.addSpeaker(speaker);
+    });
+    document.listenForRemoval(function(id) {
+        $suggestedSpeakers.find("[data-id='"+id+"']").removeClass('disabled');
     });
 });
