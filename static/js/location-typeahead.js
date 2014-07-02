6b136994155cc35bae2f9edff55cc718b5ec8daa
David King <dave@davbo.org>
2014-07-02 17:13:32 +0100

    Refector the listener, add organisation typeahead

--- static/js/location-typeahead.js
+++ static/js/location-typeahead.js
@@ -1,4 +1,31 @@
 $(function() {
+
+
+    function TypeaheadListener() {
+        this.listen = function(typeaheadEl, targetEl, formGroupEl) {
+            var $targetEl = $(targetEl);
+            console.log($targetEl);
+            var $typeaheadEl = $(typeaheadEl);
+            var $typeaheadFormGroup = $(formGroupEl);
+            var suggestedName = '';
+            $typeaheadEl.on('typeahead:selected typeahead:autocompleted', function(ev, suggestion) {
+                suggestedName = suggestion.name;
+                $targetEl.val(suggestion.id);
+            });
+            $typeaheadEl.on('change', function(ev) {
+                if (ev.target.value===suggestedName) {
+                    $typeaheadFormGroup.removeClass('has-error');
+                    $typeaheadFormGroup.addClass('has-success');
+                } else if (ev.target.value==="") {
+                    $typeaheadFormGroup.removeClass('has-success');
+                    $typeaheadFormGroup.removeClass('has-error');
+                } else {
+                    $typeaheadFormGroup.removeClass('has-success');
+                    $typeaheadFormGroup.addClass('has-error');
+                }
+            });
+        };
+    }
     var locationBH = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -10,9 +37,8 @@ $(function() {
         }
     });
     locationBH.initialize();
-
-    $typeaheadEl = $('.js-location-typeahead');
-    $typeaheadEl.typeahead({
+    $locationTypeahead = $('.js-location-typeahead');
+    $locationTypeahead.typeahead({
         hint: true,
         highlight: true,
         minLength: 1
@@ -25,23 +51,34 @@ $(function() {
             suggestion: _.template('<p><%= name %></p>'),
         },
     });
-    var suggestedName = '';
-    $typeaheadEl.on('typeahead:selected typeahead:autocompleted', function(ev, suggestion) {
-        suggestedName = suggestion.name;
-        $('.js-location').val(suggestion.id);
-    });
-    $typeaheadFormGroup = $typeaheadEl.parentsUntil('.form-group');
-    $typeaheadEl.on('change', function(ev) {
-        if (ev.target.value===suggestedName) {
-            $typeaheadFormGroup.removeClass('has-error');
-            $typeaheadFormGroup.addClass('has-success');
-        } else if (ev.target.value==="") {
-            $typeaheadFormGroup.removeClass('has-success');
-            $typeaheadFormGroup.removeClass('has-error');
-        } else {
-            $typeaheadFormGroup.removeClass('has-success');
-            $typeaheadFormGroup.addClass('has-error');
+    var locationListener = new TypeaheadListener();
+    locationListener.listen($locationTypeahead, $('.js-location'), $locationTypeahead.parent().parent().parent());
+
+    var organisationBH = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: document.organisationConfig.suggestURL+"q=%QUERY",
+            filter: function(response) {
+                return response._embedded.pois;
+            }
         }
     });
-
+    organisationBH.initialize();
+    $organisationTypeahead = $('.js-organisation-typeahead');
+    $organisationTypeahead.typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'location',
+        displayKey: 'name',
+        source: organisationBH.ttAdapter(),
+        templates: {
+            suggestion: _.template('<p><%= name %></p>'),
+        },
+    });
+    var organisationListener = new TypeaheadListener();
+    organisationListener.listen($organisationTypeahead, $('.js-organisation'), $organisationTypeahead.parent().parent().parent());
 });
