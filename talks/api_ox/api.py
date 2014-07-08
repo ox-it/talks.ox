95ddb73dad37cd67dc3a6cda69c1e50a75671a4f
David King <dave@davbo.org>
2014-07-08 18:03:56 +0100

    Improved API to access the API

--- talks/api_ox/api.py
+++ talks/api_ox/api.py
@@ -2,13 +2,28 @@ import logging
 
 import requests
 from requests.exceptions import RequestException
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
 
+class JSONAttribute(object):
+    def __init__(self, attribute):
+        self.attribute = attribute
+
+    def __get__(self, instance, owner):
+        return instance._json.get(self.attribute, None)
+
+    def __set__(self, *args):
+        # Data descriptor
+        raise AttributeError("This is a data descriptor!")
+
+
 class ApiOxResource(object):
 
-    def __init__(self, base_url, timeout=1):
+    _json = {}
+
+    def __init__(self, base_url=settings.API_OX_URL, timeout=1):
         self.base_url = base_url
         self.timeout = timeout
 
@@ -16,7 +31,8 @@ class ApiOxResource(object):
         try:
             r = requests.get('{base_url}{path}'.format(base_url=self.base_url, path=path), timeout=self.timeout)
             if r.status_code == requests.codes.ok:
-                return r.json()
+                self._json = r.json()
+                return r
             elif r.status_code == requests.codes.not_found:
                 raise ApiNotFound()
             else:
@@ -30,12 +46,18 @@ class ApiOxResource(object):
 class PlacesResource(ApiOxResource):
 
     PATH = '/places/'
+    name = JSONAttribute('name')
+    address = JSONAttribute('address')
 
-    def get_by_id(self, ident):
-        return self._get_request('{path}{ident}'.format(path=self.PATH, ident=ident))
-
-    def get_organisation_descendants(self, ident):
-        return self._get_request('{path}{ident}/organisation-descendants'.format(path=self.PATH, ident=ident))
+    @classmethod
+    def from_identifier(cls, ident, **kwargs):
+        """Create a new instance of `cls` and call _get_request with our
+        provided identifier.
+        """
+        places_resource = cls(**kwargs)
+        places_resource._get_request('{path}{ident}'.format(
+            path=places_resource.PATH, ident=ident))
+        return places_resource
 
 
 class DatesResource(ApiOxResource):
