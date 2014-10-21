7b72a01201303f832bb6d0712c9d9cc438e6c662
Francis Golinski <f.golinski@gmail.com>
2014-10-21 18:18:23 +0100

    some add talk view tests (now failing)

--- talks/events/tests.py
+++ talks/events/tests.py
@@ -1,8 +1,9 @@
 import unittest
+from operator import attrgetter
 
 from django.test import TestCase
 
-from . import forms
+from . import forms, models, factories
 
 
 class TestEventForm(TestCase):
@@ -101,3 +102,127 @@ class TestEventGroupForm(TestCase):
         self.assertIn('title', errors)
         self.assertIn('description', errors)
 
+
+
+
+class TestCreateEventView(TestCase):
+
+    def test_get_happy_no_group_id(self):
+        response = self.client.get('/events/new')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/create_event.html')
+        self.assertContains(response, "Oxford Talks")
+        self.assertContains(response, "Add Talk")
+        self.assertIn('event_form', response.context)
+        self.assertIn('event_group_form', response.context)
+        self.assertIn('speaker_form', response.context)
+
+    def test_get_nonexistent_group(self):
+        response = self.client.get('/events/groups/8475623/new')
+        self.assertEquals(response.status_code, 404)
+        self.assertTemplateNotUsed(response, 'events/create_event.html')
+
+    def test_get_happy_for_existing_group(self):
+        group = factories.EventGroupFactory.create()
+        response = self.client.get('/events/groups/%s/new' % group.pk)
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('event_group_form', response.context)
+        self.assertEquals(response.context['event_group_form']['event_group_select'].value(), group.pk)
+
+    def test_post_valid_save_and_continue_no_group_id(self):
+        title = u'cjwnf887y98fw'
+        description = u'kfjdnsf'
+        data = {
+            'event-description ': description,
+            'another': u'true',
+            'event-title': title,
+            'event-location ': u'',
+            'event-department_suggest ': u'',
+            'email_address ': u'',
+            'event-start': u'',
+            'event-topics ': u'',
+            'event-speaker_suggest ': u'',
+            'event-group-description': u'',
+            'event-location_suggest ': u'',
+            'event-department_organiser ': u'',
+            'event-group-event_group_select ': u'',
+            'event-speakers ': u'',
+            'event-group-group_type ': u'',
+            'csrfmiddlewaretoken': u'3kHyJXv0HDO8sJPLlpvQhnBqM04cIJAM',
+            'event-group-select_create ': u'CRE',
+            'event-topic_suggest': u'',
+            'event-end ': u'',
+            'event-group-title ': u'',
+            'name ': u'',
+        }
+
+        response = self.client.post('/events/new', data)
+        self.assertRedirects(response, '/events/new')
+        count = models.Event.objects.filter(title=title, description=description).count()
+        self.assertEquals(count, 1, msg="Event instance was not saved")
+
+    def test_post_valid_save_and_continue_with_group_id(self):
+        title = u'cjwnf887y98fw'
+        description = u'kfjdnsf'
+        group = factories.EventGroupFactory.create()
+        group_id = group.pk
+        data = {
+            'event-description ': description,
+            'another': u'true',
+            'event-title': title,
+            'event-location ': u'',
+            'event-department_suggest ': u'',
+            'email_address ': u'',
+            'event-start': u'',
+            'event-topics ': u'',
+            'event-speaker_suggest ': u'',
+            'event-group-description': u'',
+            'event-location_suggest ': u'',
+            'event-department_organiser ': u'',
+            'event-group-event_group_select ': u'',
+            'event-speakers ': u'',
+            'event-group-group_type ': u'',
+            'csrfmiddlewaretoken': u'3kHyJXv0HDO8sJPLlpvQhnBqM04cIJAM',
+            'event-group-select_create ': u'SEL',
+            'event-topic_suggest': u'',
+            'event-end ': u'',
+            'event-group-title ': u'',
+            'name ': u'',
+        }
+
+        response = self.client.post('/events/groups/%s/new' % group_id, data)
+        self.assertRedirects(response, '/events/groups/%s/new' % group_id)
+        count = models.Event.objects.filter(title=title, description=description, group_id=group_id).count()
+        self.assertEquals(count, 1, msg="Event instance was not saved")
+
+    def test_post_valid(self):
+        title = u'cjwnf887y98fw'
+        description = u'kfjdnsf'
+        data = {
+            'event-description ': description,
+            'event-title': title,
+            'event-location ': u'',
+            'event-department_suggest ': u'',
+            'email_address ': u'',
+            'event-start': u'',
+            'event-topics ': u'',
+            'event-speaker_suggest ': u'',
+            'event-group-description': u'',
+            'event-location_suggest ': u'',
+            'event-department_organiser ': u'',
+            'event-group-event_group_select ': u'',
+            'event-speakers ': u'',
+            'event-group-group_type ': u'',
+            'csrfmiddlewaretoken': u'3kHyJXv0HDO8sJPLlpvQhnBqM04cIJAM',
+            'event-group-select_create ': u'CRE',
+            'event-topic_suggest': u'',
+            'event-end ': u'',
+            'event-group-title ': u'',
+            'name ': u'',
+        }
+        response = self.client.post('/events/new', data)
+        try:
+            event = models.Event.objects.get(title=title, description=description)
+        except models.Event.DoesNotExist:
+            self.fail("Event instance was not saved")
+        self.assertRedirects(response, event.get_absolute_url())
