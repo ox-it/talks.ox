8770429b2327a02734b68e8b2d8023a5ffe0dce1
Martin Filliau <martin@filliau.com>
2014-10-27 15:37:22 +0000

    Fixed tests

--- talks/old_talks/tests.py
+++ talks/old_talks/tests.py
@@ -8,7 +8,10 @@ class TestOldTalks(TestCase):
 
     def test_talk(self):
         event = Event()
-        event.title = "TITLE"
+        event.title = u"TITLE"
+        event.save()
 
         data = event_to_old_talk(event)
+        d = dict(data)
         self.assertEquals(len(data), 2)
+        self.assertEquals(d["talk[title]"], event.title)
