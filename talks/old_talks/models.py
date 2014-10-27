2b50c362344ccc4a9f50e19369cd0cdd7fd05769
Martin Filliau <martin@filliau.com>
2014-10-27 17:59:40 +0000

    Keep track of old talks to be able to update them

--- talks/old_talks/models.py
+++ talks/old_talks/models.py
@@ -11,19 +11,39 @@ from talks.events.models import Event
 logger = logging.getLogger(__name__)
 
 
+class OldTalk(models.Model):
+    event = models.ForeignKey(Event, null=False)
+    old_talk_id = models.CharField(max_length=20, null=False, blank=False)
+
+
 @receiver(models.signals.post_save, sender=Event)
 def update_old_talks(sender, instance, created, **kwargs):
     if hasattr(settings, "OLD_TALKS_SERVER") and hasattr(settings, "OLD_TALKS_USER") and hasattr(settings, "OLD_TALKS_PASSWORD"):
         data = event_to_old_talk(instance)
 
-        url = "{server}/talk/update/".format(server=settings.OLD_TALKS_SERVER)
+        old_talk, is_new = OldTalk.objects.get_or_create(event=instance)
+
+        if is_new:
+            url = "{server}/talk/update/".format(server=settings.OLD_TALKS_SERVER)
+        else:
+            url = "{server}/talk/update/{id}".format(server=settings.OLD_TALKS_SERVER,
+                                                     id=old_talk.old_talk_id)
 
         logger.debug("POSTing {data} to {url}".format(data=data, url=url))
 
         response = requests.post(url, data, auth=(settings.OLD_TALKS_USER, settings.OLD_TALKS_PASSWORD),
                                  allow_redirects=True, stream=False, headers={"Accept": "application/xml"})
 
-        if response.status_code != 200:
+        if response.status_code == 200:
+            if is_new:
+                if 'location' in response.headers:
+                    talk_url = response.headers['location']
+                    talk_id = talk_url.split("/")[-1]
+                    old_talk.old_talk_id = talk_id
+                    old_talk.save()
+                else:
+                    raise Exception("Didn't got the location header so cannot say which talk this is")
+        else:
             raise Exception(response.status_code)
 
 
