479ccba320fbff0630f48719415bd021ab4adcdd
Martin Filliau <martin@filliau.com>
2014-10-27 16:46:55 +0000

    test speakers

--- talks/old_talks/tests.py
+++ talks/old_talks/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from talks.events.models import Event
+from talks.events.models import Event, Speaker
 from .models import event_to_old_talk
 
 
@@ -11,7 +11,20 @@ class TestOldTalks(TestCase):
         event.title = u"TITLE"
         event.save()
 
+        s1 = Speaker()
+        s1.name = "A"
+        s1.save()
+
+        s2 = Speaker()
+        s2.name = "B"
+        s2.save()
+
+        event.speakers.add(s1, s2)
+        event.save()
+
         data = event_to_old_talk(event)
         d = dict(data)
-        self.assertEquals(len(data), 2)
+
+        self.assertEquals(len(data), 3)
         self.assertEquals(d["talk[title]"], event.title)
+        self.assertEquals(d["talk[name_of_speaker]"], "A, B")
