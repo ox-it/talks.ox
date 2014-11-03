5eb45bfd1aea8bee9f5762b76396089da7cbee5b
Martin Filliau <martin@filliau.com>
2014-11-03 13:36:59 +0000

    Fixed pattern for details of a revision

--- talks/audit_trail/urls.py
+++ talks/audit_trail/urls.py
@@ -5,5 +5,5 @@ from .views import database_usage, revision_details
 
 urlpatterns = patterns('audit',
     url(r'^$', database_usage, name='list'),
-    url(r'^/(?P<revision_id>\d+)/$', revision_details, name='details'),
+    url(r'^(?P<revision_id>\d+)/$', revision_details, name='details'),
 )
