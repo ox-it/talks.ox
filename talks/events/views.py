272351392fb21598ce74e29ab9c789dfe3c13f5b
Andrew Haith <andyhaith@gmail.com>
2014-12-03 14:33:24 +0000

    superusers see all events for 'editable by me'

--- talks/events/views.py
+++ talks/events/views.py
@@ -274,7 +274,10 @@ def contributors_events(request):
     args = {'count': count}
 
     if events_editable:
-        events = Event.objects.filter(editor_set__in=[request.user])
+        if request.user.is_superuser:
+            events = Event.objects.all()
+        else:
+            events = Event.objects.filter(editor_set__in=[request.user])
         args['editable'] = 'true'
     else:
         events = Event.objects.all()
