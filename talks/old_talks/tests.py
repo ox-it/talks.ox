cd8f2908ba47d9cc85a93bdfec868d7ba9a80b4a
Martin Filliau <martin@filliau.com>
2015-01-07 14:56:43 +0000

    Fixed tests broken after 8875a82

--- talks/old_talks/tests.py
+++ talks/old_talks/tests.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.test import TestCase
 
 from talks.events.models import (Event, Person, PersonEvent,
-                                 ROLES_SPEAKER, EventGroup)
+                                 ROLES_SPEAKER, EventGroup, EVENT_PUBLISHED)
 from .models import (event_to_old_talk, group_to_old_series,
                      get_list_id)
 
@@ -14,6 +14,7 @@ class TestOldTalks(TestCase):
         event.title = u"TITLE"
         event.start = datetime(2014, 1, 1, 10, 30)
         event.end = datetime(2014, 1, 1, 11, 30)
+        event.embargo = True
         event.save()
 
         s1 = Person()
@@ -32,13 +33,14 @@ class TestOldTalks(TestCase):
         data = event_to_old_talk(event, None)
         d = dict(data)
 
-        self.assertEquals(len(data), 7)
+        self.assertEquals(len(data), 8)
         self.assertEquals(d["talk[title]"], event.title)
         self.assertEquals(d["talk[name_of_speaker]"], "A, B")
         self.assertEquals(d["talk[abstract]"], "")
         self.assertEquals(d["talk[date_string]"], "2014/01/01")
         self.assertEquals(d["talk[start_time_string]"], "10:30")
         self.assertEquals(d["talk[end_time_string]"], "11:30")
+        self.assertEquals(d["talk[ex_directory]"], "1")
 
     def test_event_with_group_to_old_talk(self):
         event = Event()
@@ -46,6 +48,7 @@ class TestOldTalks(TestCase):
         event.description = "description"
         event.start = datetime(2014, 1, 1, 11, 00)
         event.end = datetime(2014, 1, 1, 12, 00)
+        event.status = EVENT_PUBLISHED
         event.save()
 
         s = Person()
@@ -57,7 +60,7 @@ class TestOldTalks(TestCase):
         data = event_to_old_talk(event, "22")
         d = dict(data)
 
-        self.assertEquals(len(data), 8)
+        self.assertEquals(len(data), 9)
         self.assertEquals(d["talk[title]"], event.title)
         self.assertEquals(d["talk[series_id_string]"], "22")
         self.assertEquals(d["talk[abstract]"], event.description+"\n")
@@ -65,6 +68,7 @@ class TestOldTalks(TestCase):
         self.assertEquals(d["talk[date_string]"], "2014/01/01")
         self.assertEquals(d["talk[start_time_string]"], "11:00")
         self.assertEquals(d["talk[end_time_string]"], "12:00")
+        self.assertEquals(d["talk[ex_directory]"], "0")
 
     def test_group_to_old_series(self):
         group = EventGroup()
