ox-it/talks.ox

commit b5be8dc297b4be55c2c41403eac7a523eb5dae03

Fix migrations which were referencing former api_ox and breaking on postgresql

talks/events/migrations/0001_squashed_0019_auto_20150107_1008.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('contenttypes', '__first__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api_ox', '__first__'),
     ]
 
     operations = [
@@ -49,8 +48,8 @@ class Migration(migrations.Migration):
                 ('slug', models.SlugField()),
                 ('description', models.TextField()),
                 ('group', models.ForeignKey(to_field='id', blank=True, to='events.EventGroup', null=True)),
-                ('location', models.ForeignKey(to_field='id', blank=True, to='api_ox.Location', null=True)),
-                ('department_organiser', models.ForeignKey(to_field='id', blank=True, to='api_ox.Organisation', null=True)),
+                ('location', models.TextField(default=b'', blank=True)),
+                ('department_organiser', models.TextField(default=b'', blank=True)),
             ],
             options={
             },
@@ -125,7 +124,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='cost',
-            field=models.TextField(default=b'', help_text=b'If applicable', verbose_name=b'Cost', blank=True),
+            field=models.TextField(default=b'', help_text=b'If _oplicable', verbose_name=b'Cost', blank=True),
             preserve_default=True,
         ),
         migrations.AddField(
@@ -169,18 +168,6 @@ class Migration(migrations.Migration):
             name='uri',
             field=models.URLField(default='', db_index=True),
             preserve_default=False,
-        ),
-        migrations.AlterField(
-            model_name='event',
-            name='department_organiser',
-            field=models.TextField(default=b'', blank=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='event',
-            name='location',
-            field=models.TextField(blank=True),
-            preserve_default=True,
         ),
         migrations.AlterUniqueTogether(
             name='topicitem',

talks/api_ox/migrations/0001_initial.py
@@ -1,37 +1,0 @@
-# encoding: utf8
-from __future__ import unicode_literals
-
-from django.db import models, migrations
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Location',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('identifier', models.CharField(unique=True, max_length=50, db_index=True)),
-                ('name', models.CharField(max_length=250)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Organisation',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('identifier', models.CharField(unique=True, max_length=50, db_index=True)),
-                ('name', models.CharField(max_length=250)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-    ]

talks/api_ox/migrations/0002_auto_20141030_1816.py
@@ -1,21 +1,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models, migrations
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('events', '__first__'),
-        ('api_ox', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.DeleteModel(
-            name='Location',
-        ),
-        migrations.DeleteModel(
-            name='Organisation',
-        ),
-    ]

commit ad49f3f58271bb94c151c35d61f64f1ce888da70

Fixed help text accidentally broken in b5be8dc

talks/events/migrations/0001_squashed_0019_auto_20150107_1008.py
@@ -124,7 +124,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='cost',
-            field=models.TextField(default=b'', help_text=b'If _oplicable', verbose_name=b'Cost', blank=True),
+            field=models.TextField(default=b'', help_text=b'If applicable', verbose_name=b'Cost', blank=True),
             preserve_default=True,
         ),
         migrations.AddField(