fff2ab22b19f6b92dff23a1f4e3bca7cb50b63f0
Martin Filliau <martin@filliau.com>
2015-01-27 11:24:55 +0000

    Refactor the query parameters in API searh view

--- talks/api/views.py
+++ talks/api/views.py
@@ -115,21 +115,18 @@ def api_event_search(request):
     if to_date:
         queries.append(Q(start__lt=to_date))
 
-    speakers = request.GET.getlist("speaker")
-    if speakers:
-        queries.append(Q(personevent__role=ROLES_SPEAKER, personevent__person__slug__in=speakers))
+    # map between URL query parameters and their corresponding django ORM query
+    list_parameters = {
+        'speaker': lambda speakers: Q(personevent__role=ROLES_SPEAKER, personevent__person__slug__in=speakers),
+        'venue': lambda venues: Q(location__in=venues),
+        'organising_department': lambda depts: Q(department_organiser__in=depts),
+        'topic': lambda topics: Q(topics__uri__in=topics)
+    }
 
-    venues = request.GET.getlist("venue")
-    if venues:
-        queries.append(Q(location__in=venues))
-
-    depts = request.GET.getlist("organising_department")
-    if depts:
-        queries.append(Q(department_organiser__in=depts))
-
-    topics = request.GET.getlist("topic")
-    if topics:
-        queries.append(Q(topics__uri__in=topics))
+    for url_query_parameter, orm_mapping in list_parameters.iteritems():
+        value = request.GET.getlist(url_query_parameter)
+        if value:
+            queries.append(orm_mapping(value))
 
     final_query = reduce(operator.and_, queries)
     events = Event.published.filter(final_query).order_by('start')
