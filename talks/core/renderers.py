df64b38aff56ac9b95ce291ac0cf261b0aeb7e28
Martin Filliau <martin@filliau.com>
2015-01-27 15:14:40 +0000

    Update iCal renderer

--- talks/core/renderers.py
+++ talks/core/renderers.py
@@ -7,22 +7,28 @@ class ICalRenderer(renderers.BaseRenderer):
     format = 'ical'
 
     def render(self, data, media_type=None, renderer_context=None):
-        results = data.get('results', list())
+        if '_embedded' not in data:
+            results = [data]
+        else:
+            results = data.get('_embedded', list())
         cal = Calendar()
         cal.add('prodid', 'talks.ox.ac.uk')
         cal.add('version', '2.0')
 
         for e in results:
-            event = Event()
-            if 'title' in e:
-                event.add('summary', e['title'])
-            if 'description' in e:
-                event.add('description', e['description'])
-            if 'start' in e:
-                event.add('dtstart', e['start'])
-            if 'url' in e:
-                event.add('url', e['url'])
-                event.add('uid', e['url'])
-            cal.add_component(event)
-
+            cal.add_component(self._event_to_ics(e))
         return cal.to_ical()
+
+    @staticmethod
+    def _event_to_ics(e):
+        event = Event()
+        if 'title' in e:
+            event.add('summary', e['title'])
+        if 'description' in e:
+            event.add('description', e['description'])
+        if 'start' in e:
+            event.add('dtstart', e['start'])
+        if 'url' in e:
+            event.add('url', e['url'])
+            event.add('uid', e['url'])
+        return event
--- talks/core/tests.py
+++ talks/core/tests.py
@@ -28,6 +28,25 @@ class ICalSerializerTest(TestCase):
         self.assertEquals(cal.subcomponents[1]['DESCRIPTION'], '')
         self.assertEquals(cal.subcomponents[1]['URL'], 'http://oxtalks.com/test/2')
 
+    def test_event_to_ics(self):
+        renderer = ICalRenderer()
+        events = [{'url': 'http://oxtalks.com/test/1',
+                   'title': 'Talk 1',
+                   'description': 'Description'},
+                  {'url': 'http://oxtalks.com/test/2',
+                   'title': 'Talk 2',
+                   'description': ''}]
+        data = renderer.render({'results': events})
+        cal = Calendar.from_ical(data)
+        print cal
+        self.assertEquals(len(cal.subcomponents), 2)
+        self.assertEquals(cal.subcomponents[0]['SUMMARY'], 'Talk 1')
+        self.assertEquals(cal.subcomponents[0]['DESCRIPTION'], 'Description')
+        self.assertEquals(cal.subcomponents[0]['URL'], 'http://oxtalks.com/test/1')
+        self.assertEquals(cal.subcomponents[1]['SUMMARY'], 'Talk 2')
+        self.assertEquals(cal.subcomponents[1]['DESCRIPTION'], '')
+        self.assertEquals(cal.subcomponents[1]['URL'], 'http://oxtalks.com/test/2')
+
 
 class UtilsParseDate(TestCase):
 
