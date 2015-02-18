1e8e1e374c76c3d6def19ca34b24e59bcd5b3074
Andrew Haith <andyhaith@gmail.com>
2015-02-18 12:35:31 +0000

    Amend old_talks tests for #267

--- talks/old_talks/tests.py
+++ talks/old_talks/tests.py
@@ -44,7 +44,8 @@ class TestOldTalks(TestCase):
         data = event_to_old_talk(event, None)
         d = dict(data)
 
-        self.assertEquals(len(data), 8)
+        self.assertEquals(len(data), 9)
+        self.assertEquals(d["talk[venue_name]"], event.location_details)
         self.assertEquals(d["talk[title]"], event.title)
         self.assertEquals(d["talk[name_of_speaker]"], "A (Abio), B (Bbio)")
         self.assertTrue("Email address for booking: test@test.com" in d["talk[abstract]"])
