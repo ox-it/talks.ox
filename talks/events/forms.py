73d188688b2c487e24d8469cae5412aacf198f20
Andrew Robinson <andrew.robinson@it.ox.ac.uk>
2015-07-23 11:33:52 +0100

    Added form-control class to date widgets

--- talks/events/forms.py
+++ talks/events/forms.py
@@ -9,14 +9,14 @@ from talks.contributors.forms import BootstrappedDateTimeWidget, OxPointField
 class BrowseEventsForm(forms.Form):
     start_date = forms.DateTimeField(label="Start Date",
     								 required=False, 
-    								 widget=BootstrappedDateTimeWidget(attrs={'readonly': True}))
+    								 widget=BootstrappedDateTimeWidget(attrs={'readonly': True, 'class': 'form-control'}))
     to = forms.DateTimeField(label="End Date",
     						 required=False, 
     						 help_text="Choose date range to filter by.",
-    						 widget=BootstrappedDateTimeWidget(attrs={'readonly': True}))
+    						 widget=BootstrappedDateTimeWidget(attrs={'readonly': True, 'class': 'form-control'}))
     venue = OxPointField(datasources.LOCATION_DATA_SOURCE,
                             	label="Venue",
                             	required=False,
+#                            	attrs={'class': 'form-control'},
                             	help_text="Type building name and select from the list.")
     
- 
