74961a636c417ca6e0ed6b17ad49fb1f1d5d61ae
Andrew Robinson <andrew.robinson@it.ox.ac.uk>
2015-09-01 14:59:00 +0100

    Added migration step to convert existing incorrect talk times

---
+++ talks/events/migrations/0009_utc_timezone_adjust.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pytz
+import django.utils.timezone
+from django.db import models, migrations
+
+
+def utc_timezone_adjust(apps, schema_editor):
+    # If event time is in BST then delete an hour from it to make it UTC.
+    Event = apps.get_model('events', "Event")
+    for event in Event.objects.all():
+        # lose existing (incorrect) timezone info
+        start = django.utils.timezone.make_naive(event.start, pytz.utc)
+        end = django.utils.timezone.make_naive(event.end, pytz.utc)
+
+        # make correct time
+        event.start = pytz.timezone("Europe/London").localize(start)
+        event.end = pytz.timezone("Europe/London").localize(end)
+
+        event.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0008_auto_20150318_1624'),
+    ]
+
+    operations = [
+        migrations.RunPython(utc_timezone_adjust)
+    ]
