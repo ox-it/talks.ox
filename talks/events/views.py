ee9b1612c95118d66fd043554c330ec9e6a95178
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-04-27 17:36:39 +0100

    show host and organiser and speaker talks for a person

--- talks/events/views.py
+++ talks/events/views.py
@@ -238,19 +238,18 @@ def show_person(request, person_slug):
     host_events = events.filter(personevent__role=ROLES_HOST, personevent__person__slug=person.slug)
     speaker_events = events.filter(personevent__role=ROLES_SPEAKER, personevent__person__slug=person.slug)
     organiser_events = events.filter(personevent__role=ROLES_ORGANISER, personevent__person__slug=person.slug)
-    if host_events:
-        grouped_events = group_events(host_events)
-    elif speaker_events:
-        grouped_events = group_events(speaker_events)
-    elif speaker_events:
-        grouped_events = group_events(speaker_events)
+    grouped_host_events = group_events(host_events)
+    grouped_speaker_events = group_events(speaker_events)
+    grouped_organiser_events = group_events(organiser_events)
 
     context = {
         'person': person,
         'host_events': host_events,
         'speaker_events': speaker_events,
         'organiser_events': organiser_events,
-        'grouped_events': grouped_events,
+        'grouped_host_events': grouped_host_events,
+        'grouped_speaker_events': grouped_speaker_events,
+        'grouped_organiser_events': grouped_organiser_events,
     }
     if request.GET.get('format') == 'txt':
         return render(request, 'events/person.txt.html', context)
