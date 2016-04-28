45cec9a93427684051182640aadc3a3ecaffb27c
Andrew Haith <andyhaith@gmail.com>
2016-04-28 10:26:13 +0100

    Allow transferring ownership of a public list via the admin interface. Remove old talks from admin interface

--- talks/old_talks/admin.py
+++ talks/old_talks/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
 from .models import OldTalk, OldSeries
+# 
+# 
+# class OldTalkAdmin(admin.ModelAdmin):
+#     list_display = ('old_talk_id', 'event')
+# 
+# 
+# class OldSeriesAdmin(admin.ModelAdmin):
+#     list_display = ('old_series_id', 'group')
 
-
-class OldTalkAdmin(admin.ModelAdmin):
-    list_display = ('old_talk_id', 'event')
-
-
-class OldSeriesAdmin(admin.ModelAdmin):
-    list_display = ('old_series_id', 'group')
-
-
-admin.site.register(OldTalk, OldTalkAdmin)
-admin.site.register(OldSeries, OldSeriesAdmin)
+# 
+# admin.site.register(OldTalk, OldTalkAdmin)
+# admin.site.register(OldSeries, OldSeriesAdmin)
