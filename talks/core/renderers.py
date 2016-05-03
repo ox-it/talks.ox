3df7b729d7deb7a151c591d22b100aac3e62cda6
Andrew Haith <andyhaith@gmail.com>
2016-05-03 13:14:28 +0100

    Fix merge error.

--- talks/core/renderers.py
+++ talks/core/renderers.py
@@ -28,16 +28,7 @@ class ICalRenderer(renderers.BaseRenderer):
         event.add('summary', e['title_display'])
         
         if 'description' in e:
-            if len(e['description']):
-                desc_with_speakers = e['description']
-                speakers_list = ""
-                if 'various_speakers' in e and e['various_speakers'] is True:
-                    speakers_list = "\nSpeakers:\n Various"
-                elif 'speakers' in e:
-                    if len(e['speakers']):
-                        speakers_list = "\nSpeakers:\n" + ", ".join(get_speaker_name(speaker) for speaker in e['speakers'])
-                event.add('description', desc_with_speakers + speakers_list)
-            
+            print 'got a description'
             desc_status = "" 
             if 'status' in e:
                 if e['status'] == 'preparation':
@@ -50,8 +41,8 @@ class ICalRenderer(renderers.BaseRenderer):
             elif 'speakers' in e:
                 if len(e['speakers']):
                     speakers_list = "\nSpeakers:\n" + ", ".join(get_speaker_name(speaker) for speaker in e['speakers'])
-            event.add('description', desc_status + desc_with_speakers + speakers_list)
-        
+            event.add('description', desc_status + desc_with_speakers + speakers_list)            
+            
         if 'start' in e:
             event.add('dtstart', dt_string_to_object(e['start']))
         if 'end' in e:
