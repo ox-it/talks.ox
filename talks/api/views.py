15c4c65eab89002cdb90eaf07fede9c40d3ebb84
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-05-04 09:19:49 +0100

    clean event description before showing xml

--- talks/api/views.py
+++ talks/api/views.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import date
-
+from re import sub, compile
+from sys import maxunicode
 from django.contrib.auth.models import User
 
 from django.core.exceptions import ObjectDoesNotExist
@@ -178,8 +179,33 @@ def api_event_get(request, slug):
     if not event:
         raise Http404
     serializer = HALEventSerializer(event, read_only=True, context={'request': request})
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    data = serializer.data
+    # data['description'] = sub(ur'[\x1F00]+', '', data['description'])
+    print "- - - - --- -- - - - - data:", data
+    data['description'] = clean_xml(data['description'])
+    data['cost'] = clean_xml(data['cost'])
+    # for 
+    return Response(data, status=status.HTTP_200_OK)
 
+def clean_xml(dirty):
+    _illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F), 
+                            (0x7F, 0x84), (0x86, 0x9F), 
+                            (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)] 
+    if maxunicode >= 0x10000:  # not narrow build 
+            _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), 
+                                     (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF), 
+                                     (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF), 
+                                     (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), 
+                                     (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF), 
+                                     (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF), 
+                                     (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), 
+                                     (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)]) 
+
+    _illegal_ranges = ["%s-%s" % (unichr(low), unichr(high)) 
+                       for (low, high) in _illegal_unichrs] 
+    _illegal_xml_chars_RE = compile(u'[%s]' % u''.join(_illegal_ranges))
+    clean = _illegal_xml_chars_RE.sub('', dirty)
+    return clean
 
 @api_view(["GET"])
 @renderer_classes((ICalRenderer,))
