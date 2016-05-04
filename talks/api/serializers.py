038bd558947cba5c50ad95e15dc0df32ef32d7b9
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-05-04 14:55:42 +0100

    simplify EventLinksSerializer class

--- talks/api/serializers.py
+++ talks/api/serializers.py
@@ -90,37 +90,10 @@ class HALURICharField(Field):
 
 
 class EventLinksSerializer(serializers.ModelSerializer):
-    self = serializers.SerializerMethodField()
-    talks_page = serializers.SerializerMethodField()
-    ics = serializers.SerializerMethodField()
+    self = HALURICharField(source='get_api_url', read_only=True)
+    talks_page = HALURICharField(source='get_absolute_url', read_only=True)
+    ics = HALURICharField(source='get_ics_url', read_only=True)
     
-    def get_self(self, obj):
-        if 'request' in self.context:
-            req = self.context.get('request')
-            url = req.build_absolute_uri(obj.get_api_url())
-        else:
-            url = obj.get_api_url()
-        field = HALURICharField()
-        return field.to_representation(url)
-
-    def get_talks_page(self, obj):
-        if 'request' in self.context:
-            req = self.context.get('request')
-            url = req.build_absolute_uri(obj.get_absolute_url())
-        else:
-            url = obj.get_absolute_url()
-        field = HALURICharField()
-        return field.to_representation(url)
-
-    def get_ics(self, obj):
-        if 'request' in self.context:
-            req = self.context.get('request')
-            url = req.build_absolute_uri(obj.get_ics_url())
-        else:
-            url = obj.get_ics_url()
-        field = HALURICharField()
-        return field.to_representation(url)
-
     class Meta:
         model = Event
         fields = ('self', 'talks_page', 'ics')
