fbf570a05c50d9149c3eb4f96c0c45ac57fd4139
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-05-05 16:01:04 +0100

    search: format date to oxford-date

--- talks/events_search/views.py
+++ talks/events_search/views.py
@@ -5,7 +5,7 @@ Custom search view, includes our custom dynamic faceting
 from haystack.views import FacetedSearchView
 from datetime import datetime
 
-from talks.events.views import group_events
+from talks.events.views import group_events, date_to_oxford_date
 
 
 class StartDateFacetItem(object):
@@ -68,6 +68,18 @@ class SearchUpcomingView(SearchView):
         extra = super(SearchUpcomingView, self).extra_context()
         
         extra['top_results'] = self.get_results()[:5]
+        for top_event in extra['top_results']:
+            ox_date = date_to_oxford_date(top_event.start)
+            comps = ox_date.components
+            date_str = comps['day_name']+ " " +str(comps['day_number'])+ " " +comps['month_long']+ " "
+            date_str+= str(comps['year'])+ " ("+ str(comps['week']) + comps['ordinal']+ " Week, " +comps['term_long']+ " Term)"
+            hours = datetime.strftime(top_event.start, '%I')
+            minutes = datetime.strftime(top_event.start, ':%M')
+            if minutes==":00":
+                minutes = ""
+            ampm = datetime.strftime(top_event.start, '%p')
+            top_event.oxford_date_time = date_str +" "+ str(int(hours))+minutes+ampm.lower()
+
         now = datetime.now()
         extra['future_results'] = self.get_results().filter(start__gte=now).order_by('start')
 
