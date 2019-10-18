e8f759d96835c92c9894a01fda48a65f162ff03f
Finlay Birnie <finlay.birnie@it.ox.ac.uk>
2019-10-18 14:26:11 +0100

    PersistentRemoteUserMiddleware not available in this django version

--- talks/settings.py
+++ talks/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
