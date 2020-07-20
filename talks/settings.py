3c2a06b80399fdb7d8c9e854f681e05fbbd35e4f
birniefoxford <finlay.birnie@it.ox.ac.uk>
2020-07-20 11:46:38 +0100

    use newer middleware declaration

--- talks/settings.py
+++ talks/settings.py
@@ -84,7 +84,7 @@ INSTALLED_APPS = (
     'talks.old_talks'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
